dfd8eaefbd55d7f96cbbfc591b802de79a7ebc8e
Joshua Dave Oropilla <joshdave900@gmail.com>
2026-02-27 02:18:22 +0800

    chore: Install dependencies for the new qr-api service and remove the api-gateway index file.

--- apis/api-gateway/src/index.ts
+++
@@ -1,9 +0,0 @@
-import http from "node:http";
-import app from "./app";
-import { env } from "./config";
-
-const server = http.createServer(app);
-
-server.listen(env.PORT, () => {
-    console.log(`Server is running on port ${env.PORT}`);
-});
--- apis/api-gateway/src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import mainRoutes from "./main";
-
-const router = Router();
-
-router.use("/", mainRoutes);
-
-export default router;
