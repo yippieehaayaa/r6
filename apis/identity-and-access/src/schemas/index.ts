6c0a534cdb70a52957059bb32f75c69eb21dc62f
Joshua Dave Oropilla <joshdave900@gmail.com>
2026-03-07 23:08:45 +0800

    refactor: remove unused group, otp, and service client schemas

--- apis/identity-and-access/src/schemas/index.ts
+++ apis/identity-and-access/src/schemas/index.ts
@@ -1,6 +1,4 @@
 export * from "./identity.js";
-export * from "./otp.js";
 export * from "./policy.js";
 export * from "./role.js";
-export * from "./service-client.js";
 export * from "./session.js";
