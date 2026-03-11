ceb711e19c0df369788a085f43603e68e5eb295f
Joshua Dave Oropilla <joshdave900@gmail.com>
2026-03-11 18:26:19 +0800

    feat: refactor error handling and import paths in brand and category management files

--- apis/inventory-and-catalog/src/models/catalog/brand/getBrandById.ts
+++ apis/inventory-and-catalog/src/models/catalog/brand/getBrandById.ts
@@ -1,14 +1,14 @@
-import { BrandNotFoundError } from "../../../errors";
+import { BrandNotFoundError } from "../../../utils/errors";
 import { prisma } from "../../../utils/prisma";
 
 const getBrandById = async (id: string) => {
-  const brand = await prisma.brand.findUnique({
-    where: { id, deletedAt: { isSet: false } },
-  });
+	const brand = await prisma.brand.findUnique({
+		where: { id, deletedAt: { isSet: false } },
+	});
 
-  if (!brand) throw new BrandNotFoundError();
+	if (!brand) throw new BrandNotFoundError();
 
-  return brand;
+	return brand;
 };
 
 export default getBrandById;
