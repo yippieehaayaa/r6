3d8f876c97290880266ecca5ecc946ea13f64914
Joshua Dave Oropilla <joshdave900@gmail.com>
2026-03-12 07:35:49 +0800

    feat(analytics): add overview analytics functions for dead stock, GMV, pre-season inventory health, seasonal demand, and supplier fill rate

---
+++ apis/inventory-and-catalog/src/models/analytics/overview/index.ts
@@ -0,0 +1,5 @@
+export { default as getDeadStockReport } from "./getDeadStockReport";
+export { default as getGmv } from "./getGmv";
+export { default as getPreSeasonInventoryHealth } from "./getPreSeasonInventoryHealth";
+export { default as getSeasonalDemandReport } from "./getSeasonalDemandReport";
+export { default as getSupplierFillRate } from "./getSupplierFillRate";
