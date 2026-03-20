67c7c06b41bcca99385af2a1fb225178d8d82c46
yippieehaayaa <joshdave900@gmail.com>
2026-03-20 14:21:28 +0800

    feat: update baseURL for inventory-and-catalog and change dev port to 5000

--- apis/api-gateway/src/routes/index.ts
+++ apis/api-gateway/src/routes/index.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express';
+import inventoryAndCatalogMiddleware  from './inventory-and-catalog';
 
 const router = Router();
+
+router.use('/inventory-and-catalog', inventoryAndCatalogMiddleware);
 
 export default router;
