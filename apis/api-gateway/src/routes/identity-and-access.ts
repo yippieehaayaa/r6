35a9d60fc537f2d638b7cd0903ea148a3b3a22a7
Joshua Dave Oropilla <joshdave900@gmail.com>
2026-03-26 14:23:43 +0800

    feat: implement identity and access management endpoints and proxy middleware

---
+++ apis/api-gateway/src/routes/identity-and-access.ts
@@ -0,0 +1,14 @@
+import { type Request, type Response, Router } from "express";
+import { createProxyMiddleware } from "http-proxy-middleware";
+import { env } from "../config";
+
+const router: Router = Router();
+
+const middleware = createProxyMiddleware<Request, Response>({
+  target: env.IAA_URL,
+  changeOrigin: true,
+});
+
+router.use(middleware);
+
+export default router;
--- apis/api-gateway/src/routes/index.ts
+++ apis/api-gateway/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
-import inventoryAndCatalogMiddleware  from './inventory-and-catalog';
+import identityAndAccessMiddleware from './identity-and-access';
+import inventoryAndCatalogMiddleware from './inventory-and-catalog';
 
 const router = Router();
 
+router.use('/identity-and-access', identityAndAccessMiddleware);
 router.use('/inventory-and-catalog', inventoryAndCatalogMiddleware);
 
 export default router;
